dda4134ee4ca94d498d1fb3218d6c083a74baad1
Nitin Peter <154044708+NitinS47@users.noreply.github.com>
2025-05-27 11:24:53 +0530

    Delete frontend directory

--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "light",
-  setTheme: (theme) => {
-    localStorage.setItem("streamify-theme", theme);
-    set({ theme });
-  },
-}));
